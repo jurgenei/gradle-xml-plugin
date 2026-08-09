3dae77afe41166c6e725ae6d55eb86cb2b79c4a7
Jurgen Hildebrand <Jurgen.Hildebrand@ing.com>
2026-08-09 14:59:09 +0200

    fix merge conflicts

--- src/main/java/name/jurgenei/gradle/xml/AbstractXmlValidationTask.java
+++ src/main/java/name/jurgenei/gradle/xml/AbstractXmlValidationTask.java
@@ -64,16 +64,6 @@ public abstract class AbstractXmlValidationTask extends SourceTask implements Va
     @Input
     public abstract MapProperty<String, String> getParams();
 
-    @Internal
-    public abstract DirectoryProperty getProjectDir();
-
-    /**
-     * Project directory used during execution-time path resolution.
-     *
-     * @return project directory property
-     */
-    @Internal
-    public abstract DirectoryProperty getProjectDir();
 
     /**
      * Project directory used during execution-time path resolution.
